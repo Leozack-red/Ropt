8c78a4833c0d5601d3d17f04e680a4a2ed01d80f
Lev Pruglo <lev.pruglo@mail.ru>
2023-11-15 15:57:30 +0300

    add tests and change text of exception

--- lib/ropt/cooperative_games/validator.rb
+++ lib/ropt/cooperative_games/validator.rb
@@ -5,13 +5,14 @@ module Ropt
     # class for validation after and before calculating vector shapley
     class Validator
       def validate_arguments(coefficients, coalitions, number_gamers)
-        validate_coefficients(coefficients)
-        validate_coalitions(coalitions)
         validate_number_gamers(number_gamers)
+        validate_coalitions(coalitions)
+        validate_coefficients(coefficients)
       end
 
       def validate_coalitions_consistency(coalitions, names_singletons)
-        validate_consistency(coalitions, names_singletons)
+        value_grand_coalition = value_grand_coalition(coalitions, names_singletons)
+        validate_value_grand_coalition(value_grand_coalition)
       end
 
       def validate_result(coalitions, names_singletons, vector)
@@ -22,7 +23,7 @@ module Ropt
       private
 
       def validate_coefficients(coefficients)
-        raise Error, "Not find coefficients" if coefficients.empty? || coefficients.nil?
+        raise Error, "Not find coefficients" if coefficients.nil? || coefficients.empty?
       end
 
       def validate_coalitions(coalitions)
@@ -33,31 +34,29 @@ module Ropt
         raise Error, "Not find number of gamers" if number_gamers.nil?
       end
 
-      def validate_consistency(coalitions, names_singletons)
-        value_grand_coalition = value_grand_coalition(coalitions, names_singletons)
-        validate_value_grand_coalition(value_grand_coalition)
-
-        return unless Set.new(names_singletons) == Set.new(coalitions.keys.max.to_s.chars)
-
-        raise Error, "Not find gamer in grand coalition or singleton coalition"
-      end
-
       def value_grand_coalition(coalitions, names_singletons)
-        coalitions.select { |coalition| coalition.size == names_singletons.count }.values.first
+        most_big_coalition = coalitions.keys.max_by(&:length)
+        most_big_coalition_by_singles = coalitions.select do |coalition|
+          coalition.size == names_singletons.count
+        end
+
+        return nil if most_big_coalition != most_big_coalition_by_singles.keys.first
+
+        most_big_coalition_by_singles.values.first
       end
 
       def validate_value_grand_coalition(value)
         return if value
 
         raise Error,
-              "Not find value of grand coalition or no several singletons coalitions is defined"
+              "Not find value of grand coalition or several singletons coalitions"
       end
 
       def validate_vector(value_grand_coalition, vector)
         return if vector.values.sum.round(2) == value_grand_coalition.to_f.round(2)
 
         raise Error,
-              "Sum values of vector_matrix_math is not equal value of grand coalition"
+              "Sum components of Shapley vector is not equal value of grand coalition"
       end
     end
   end
--- spec/ropt/cooperative_games/shapley_value_spec.rb
+++ spec/ropt/cooperative_games/shapley_value_spec.rb
@@ -4,8 +4,101 @@ RSpec.describe Ropt::CooperativeGames::ShapleyValue do
   subject(:shapley_value) { described_class.new(coalitions, number_gamers).solve }
 
   describe "#solve" do
-    context "with 3-gamers game and integers values of costs" do
+    context "when 3-gamers game" do
       let(:number_gamers) { 3 }
+
+      context "with integers values of costs" do
+        let(:coalitions) do
+          {
+            B: 0,
+            A: 0,
+            AB: 1,
+            C: 0,
+            ABC: 1,
+            AC: 1
+          }
+        end
+
+        it "return Shapley Value" do
+          expect(shapley_value).to eq(A: (2.0 / 3.0).round(3),
+                                      B: (1.0 / 6.0).round(3),
+                                      C: (1.0 / 6.0).round(3))
+        end
+      end
+
+      context "with float values of gain" do
+        let(:number_gamers) { 3 }
+        let(:coalitions) do
+          {
+            a: 0.0,
+            b: 0.0,
+            c: 0.0,
+            ab: 1.0,
+            ac: 1.0,
+            abc: 1.0
+          }
+        end
+
+        it "return Shapley Value" do
+          expect(shapley_value).to eq(a: (2.0 / 3.0).round(3),
+                                      b: (1.0 / 6.0).round(3),
+                                      c: (1.0 / 6.0).round(3))
+        end
+      end
+
+      context "with integer values of gain" do
+        let(:coalitions) do
+          {
+            a: 5000,
+            b: 5000,
+            c: 0,
+            ab: 7500,
+            ac: 7500,
+            bc: 5000,
+            abc: 10_000
+          }
+        end
+
+        context "with more then 10 gain" do
+          it "return Shapley Value" do
+            expect(shapley_value).to eq(a: 5000, b: 3750, c: 1250)
+          end
+        end
+      end
+
+      context "with very small gains" do
+        subject(:shapley_value) { described_class.new(coalitions, number_gamers, round).solve }
+
+        let(:coalitions) do
+          {
+            a: 0.00234,
+            b: 0.2341,
+            c: 0.000001234,
+            ab: 0.753,
+            ac: 0.107539,
+            bc: 0.3453400345,
+            abc: 1.957650912873
+          }
+        end
+        let(:round) { 7 }
+
+        it "return Shapley Value" do
+          expect(shapley_value).to eq(c: 0.4376239, a: 0.6426233, b: 0.8774038)
+        end
+      end
+
+      context "with missing of coalitions of gamers" do
+        let(:number_gamers) { 3 }
+        let(:coalitions) { nil }
+
+        it "raise exception" do
+          expect { shapley_value }.to raise_error(Ropt::Error, "Not find coalitions")
+        end
+      end
+    end
+
+    context "when missing of number of gamers" do
+      let(:number_gamers) { nil }
       let(:coalitions) do
         {
           B: 0,
@@ -17,91 +110,82 @@ RSpec.describe Ropt::CooperativeGames::ShapleyValue do
         }
       end
 
-      it "return Shapley Value" do
-        expect(shapley_value).to eq(A: (2.0 / 3.0).round(3),
-                                    B: (1.0 / 6.0).round(3),
-                                    C: (1.0 / 6.0).round(3))
+      it "raise exception" do
+        expect { shapley_value }.to raise_error(Ropt::Error, "Not find number of gamers")
       end
     end
 
-    context "with 3-gamers game and float values of gain" do
-      let(:number_gamers) { 3 }
+    context "when missing coefficients" do
       let(:coalitions) do
         {
-          a: 0.0,
-          b: 0.0,
-          c: 0.0,
-          ab: 1.0,
-          ac: 1.0,
-          abc: 1.0
+          B: 0,
+          A: 0,
+          AB: 1,
+          C: 0,
+          ABC: 1,
+          AC: 1
         }
       end
-
-      it "return Shapley Value" do
-        expect(shapley_value).to eq(a: (2.0 / 3.0).round(3),
-                                    b: (1.0 / 6.0).round(3),
-                                    c: (1.0 / 6.0).round(3))
-      end
-    end
-
-    context "with 3-gamers game and integer values of gain" do
-      let(:coalitions) do
-        {
-          a: 5000,
-          b: 5000,
-          c: 0,
-          ab: 7500,
-          ac: 7500,
-          bc: 5000,
-          abc: 10_000
-        }
-      end
-
-      context "with more then 10 gain" do
-        let(:number_gamers) { 3 }
-
-        it "return Shapley Value" do
-          expect(shapley_value).to eq(a: 5000, b: 3750, c: 1250)
-        end
-      end
-
-      context "with missing of number of gamers will return error" do
-        let(:number_gamers) { nil }
-
-        it "raise exception" do
-          expect { shapley_value }.to raise_error(Ropt::Error)
-        end
-      end
-    end
-
-    context "with 3-gamers game and very small gains" do
-      subject(:shapley_value) { described_class.new(coalitions, number_gamers, round).solve }
-
       let(:number_gamers) { 3 }
-      let(:coalitions) do
-        {
-          a: 0.00234,
-          b: 0.2341,
-          c: 0.000001234,
-          ab: 0.753,
-          ac: 0.107539,
-          bc: 0.3453400345,
-          abc: 1.957650912873
-        }
-      end
-      let(:round) { 7 }
 
-      it "return Shapley Value" do
-        expect(shapley_value).to eq(c: 0.4376239, a: 0.6426233, b: 0.8774038)
+      before do
+        double = instance_double(
+          Ropt::CooperativeGames::CoalitionsCoefficients,
+          call: nil
+        )
+        allow(
+          Ropt::CooperativeGames::CoalitionsCoefficients
+        )
+          .to receive(:new)
+          .with(number_gamers)
+          .and_return(double)
       end
-    end
-
-    context "with missing of coalitions of gamers will return error" do
-      let(:number_gamers) { 3 }
-      let(:coalitions) { nil }
 
       it "raise exception" do
-        expect { shapley_value }.to raise_error(Ropt::Error)
+        expect { shapley_value }.to raise_error(Ropt::Error, "Not find coefficients")
+      end
+    end
+
+    context "when not consistency grand coalition" do
+      context "with value grand coalition as nil" do
+        let(:coalitions) do
+          {
+            B: 0,
+            A: 0,
+            AB: 1,
+            C: 0,
+            ABC: nil,
+            AC: 1
+          }
+        end
+        let(:number_gamers) { 3 }
+
+        it "raise exception" do
+          expect { shapley_value }.to raise_error(
+            Ropt::Error,
+            "Not find value of grand coalition or several singletons coalitions"
+          )
+        end
+      end
+
+      context "without one singleton coalition" do
+        let(:coalitions) do
+          {
+            B: 0,
+            AB: 1,
+            C: 0,
+            ABC: 2,
+            AC: 1
+          }
+        end
+        let(:number_gamers) { 3 }
+
+        it "raise exception" do
+          expect { shapley_value }.to raise_error(
+            Ropt::Error,
+            "Not find value of grand coalition or several singletons coalitions"
+          )
+        end
       end
     end
   end
